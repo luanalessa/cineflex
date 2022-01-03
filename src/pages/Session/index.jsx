a9fcd8603cc06c90a9fd87e13cd24e97989eca01
Luana Lessa <lessalsn@gmail.com>
2022-01-03 19:07:08 -0300

    feat: Add sessions

---
+++ src/pages/Session/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from '../../components/Header';
+import MovieSession from '../../components/MovieSession';
+
+
+export default function Session() {
+ 
+  return (
+    <>
+    <Header />
+    <MovieSession />
+
+    </>
+  )
+}
