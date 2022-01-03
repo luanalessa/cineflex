47d81e44c436aec1d913797bf486a0cb57267de0
Luana Lessa <lessalsn@gmail.com>
2022-01-03 14:23:03 -0300

    feat: Add home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+import MovieList from '../../components/MovieList';
+
+export default function Home() {
+ 
+  return (
+    <>
+    <Header />
+    <MovieList />
+
+    </>
+  )
+}
+
